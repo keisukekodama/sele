3b81f8d0ad4c78647f163c9e2dc749a4579b8a82
keisukekodama <myzk.kodama@gmail.com>
2020-02-22 20:27:04 +0900

    Update sele.rb
    ・処理ごとにputsで表示
    ・スクリーンショットできるように追記

--- sele.rb
+++ sele.rb
@@ -14,8 +14,8 @@ email = d.find_element(:id, 'user_email')
 password = d.find_element(:id, 'user_password')
 confirmation = d.find_element(:id, 'user_password_confirmation')
 
-user1="tech@camp45"
-user2="tech@camp68"
+user1="tech@camp15"
+user2="tech@camp168"
 pass="techcamp3"
 
 name.send_keys(user1)
@@ -24,20 +24,16 @@ password.send_keys(pass)
 confirmation.send_keys(pass)
 d.find_element(:name,"commit").click
 wait.until {d.find_element(:class, "fa-cog").displayed?}
-
-
-  # d.get(url+"/users/\d+\/edit").click
+# ログアウト
   d.find_element(:class, "fa-cog").click
-
   d.find_element(:class, "btn").click
   d.find_element(:class, "btn").click
-  wait.until {d.find_element(:id, 'user_name').displayed?}
-  # d.get(url+"/users/sign_up")
+wait.until {d.find_element(:id, 'user_name').displayed?}
+# サインアップ2
   name = d.find_element(:id, 'user_name')
   email = d.find_element(:id, 'user_email')
   password = d.find_element(:id, 'user_password')
   confirmation = d.find_element(:id, 'user_password_confirmation')
-  
 
   name.send_keys(user2)
   email.send_keys(user2)
@@ -45,61 +41,46 @@ wait.until {d.find_element(:class, "fa-cog").displayed?}
   confirmation.send_keys(pass)
   d.find_element(:name,"commit").click
 
-
-
-
-
-
 # グループ作成
 d.get(url+"/groups/new")
-#want グループの名前ランダムで入れる。
-if true
 wait.until {d.find_element(:id,"group_name").displayed?}
 d.find_element(:id,"group_name").send_keys('tsr')
 sleep 1
 d.find_element(:id, 'user-search-field').send_keys("t")
 sleep 1
 #want 検索のランダム化
-# wait.until { d.find_element(:class, 'user-search-add').displayed? }
 d.find_element(:class,"user-search-add").click
 d.find_element(:name,"commit").click
 sleep 1
 
-else
+puts "インクリメンタルサーチ完了"
 
-puts "インクリメンタルサーチエラー"
-end
-
-
-
-# d.find_element(:xpath, "//html/body/div[2]/div/div[2]/a[3]").click
 d.get(url+"/groups/1/messages")
-sleep 5
+sleep 3
 
 d.execute_script("window.open()") #make new tab
 d.switch_to.window(d.window_handles[1]) #switch new tab
 d.get(url+"/groups/1/messages")
 
 
-sleep 10
-# want group_idの検索
-# d.find_element(:id,"message_content").send_keys('test')
-# d.find_element(:name,"commit").click
+sleep 3
 # what メッセージ送信
 d.find_element(:id,'message_content').send_keys('tes')
 d.find_element(:name,"commit").click
-# element = driver.switch_to.active_element
-# element.send_keys('アクティブな要素に入力', :enter)
 
-sleep 5
+
+sleep 3
 
 # what 画像送信
 d.find_element(:id,"message_image").send_keys('/Users/tech-camp/Desktop/test.jpg')
 d.find_element(:name,"commit").click
-sleep 5
+puts "非同期通信でのメッセージ送信完了"
+sleep 3
 # what 画像送信＋メッセージ送信ここで連続で押すとエラーが出るため手動で押す
 d.find_element(:id,'message_content').send_keys('test')
 d.find_element(:id,"message_image").send_keys('/Users/tech-camp/Desktop/test.jpg')
 d.find_element(:name,"commit").click
+puts "自動更新が確認できればLGTM"
+d.save_screenshot("/Users/tech-camp/Desktop/a.png")
 
-sleep 30
+sleep 180
